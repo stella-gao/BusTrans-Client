d5a764c13ec9d64e86a38ce3e5d91db964fae72f
Stella Gao <stella.gao.89@gmail.com>
2016-03-28 19:07:45 -0400

    Create MainActivity.java

---
+++ BusTrans-Client-V6/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.yuan.activities;
+
+/**
+ * Created by stella on 3/21/16.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.yuan.smartbus.R;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_simple).setOnClickListener(this);
+        findViewById(R.id.btn_transit).setOnClickListener(this);
+        //findViewById(R.id.btn_alternative).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_simple) {
+            goToSimpleDirection();
+        } /*
+        else if (id == R.id.btn_transit) {
+            goToTransitDirection();
+        }
+        else if (id == R.id.btn_alternative) {
+            goToAlternativeDirection();
+        }*/
+    }
+
+    public void goToSimpleDirection() {
+        openActivity(Login.class);
+    }
+/*
+    public void goToTransitDirection() {
+        openActivity(TransitDirectionActivity.class);
+    }
+
+    public void goToAlternativeDirection() {
+        openActivity(AlternativeDirectionActivity.class);
+    }*/
+
+    public void openActivity(Class<?> cs) {
+        startActivity(new Intent(this, cs));
+    }
+}
